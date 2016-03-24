f9a8f1372cb576e633ae8db479fa9504eb1c7f6c
IscasYuchen <yuchensoso@126.com>
2016-03-24 09:59:38 +0800

    update problem 102

--- src/BinaryTreeLevelOrderTraversal.cpp
+++ src/BinaryTreeLevelOrderTraversal.cpp
@@ -14,7 +14,30 @@ BinaryTreeLevelOrderTraversal::~BinaryTreeLevelOrderTraversal()
 vector<vector<int>> BinaryTreeLevelOrderTraversal::levelOrder(TreeNode * root)
 {
 	vector<vector<int>> ret;
-	if(root == NULL)
+		queue<TreeNode*> queue;
+		vector<int> resultLevel;
+		TreeNode *temp;
+		int numLevel = 0,i;
+		if(root == NULL)
+			return ret;
+
+		queue.push(root);
+		while (!queue.empty())
+		{
+			numLevel = queue.size();
+			resultLevel.clear();
+			for (i = 0;i < numLevel;i++)
+			{
+				temp = queue.front();
+				queue.pop();
+				resultLevel.push_back(temp->val);
+				if(temp->left!=NULL)
+					queue.push(temp->left);
+				if(temp->right!=NULL)
+					queue.push(temp->right);
+			}
+			ret.push_back(resultLevel);
+		}
 		return ret;
 
 
